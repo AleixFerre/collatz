01cb6668708b2b5dc8583f3cc3a8bf87f33c67fd
Aleix Ferré <thecatalahd@gmail.com>
2024-12-31 16:24:07 +0100

    get optimized

--- index.ts
+++ index.ts
@@ -1,31 +1,39 @@
 const solutionSet = new Set<number>();
 
-function collatzValid(n: number): boolean {
+function collatzValid(n: number, i: number): boolean {
   if (solutionSet.has(n)) {
+    return true;
+  }
+
+  if (n < i) {
     return true;
   }
 
   solutionSet.add(n);
 
   if (n % 2 === 0) {
-    return collatzValid(n / 2);
+    return collatzValid(n / 2, i);
   } else {
-    return collatzValid(3 * n + 1);
+    return collatzValid(3 * n + 1, i);
   }
 }
 
 function isCollatzValidFor(n: number): boolean {
   for (let i = 1; i < n; i++) {
-    if (i % 2 === 0) continue;
-    if (!collatzValid(i)) {
+    if (i % 2 === 0) {
+      solutionSet.delete(i);
+      continue;
+    }
+    if (!collatzValid(i, i)) {
       return false;
     }
+    solutionSet.delete(i);
   }
   return true;
 }
 
 console.time("isCollatzValidFor");
-console.log(isCollatzValidFor(100000000));
+console.log(isCollatzValidFor(100));
 console.timeEnd("isCollatzValidFor");
 
 console.log(solutionSet.size);
